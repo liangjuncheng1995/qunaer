7d8386da6a9212b5a92eeca5fa4a1817f9320f81
unknown <liangjuncheng1995>
2018-11-29 22:31:36 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import home from './page/home'
+
+export default {
+  name: 'App',
+  components: {
+    home
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/page/home.vue
@@ -0,0 +1,31 @@
+<template>	
+	<div style="height: auto;">
+		<div class="home">
+			<div class="home-title">呼唤朋友来组团<br>每天瓜分百万奖金</div>
+		</div>
+		<!-- 活动进行中activity -->
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+
+	}
+</script>
+
+<style type="text/css">
+	.home {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        top: 0px;
+        background-color: #f58723;
+    }
+    .home-title {
+    	text-align: center;
+    	color: #fff;
+    	font-size: 20px;
+    	line-height: 30px;
+    	margin-top:150px;
+    }
+</style>
