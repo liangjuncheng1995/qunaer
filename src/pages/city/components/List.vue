d667f39c91867ea033c6a327add2a53d156c93c5
unknown <liangjuncheng1995>
2020-05-03 13:57:37 +0800

    better-scroll的 渲染时机的添加

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -6,7 +6,7 @@
         <div class="button-list">
           <!-- <div class="button-wrapper" v-for="item of hot" :key="item.id">
             <div class="button">{{item.name}}</div>
-          </div> -->
+          </div>-->
         </div>
       </div>
       <div class="area">
@@ -20,12 +20,13 @@
       <div class="area" :key="key" v-for="(item, key)  of cities">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom" :key="innerItem.id" v-for="innerItem of item">{{innerItem.name}}</div>
-          
+          <div
+            class="item border-bottom"
+            :key="innerItem.id"
+            v-for="innerItem of item"
+          >{{innerItem.name}}</div>
         </div>
       </div>
-     
-     
     </div>
   </div>
 </template>
@@ -39,7 +40,9 @@ export default {
     cities: Array
   },
   mounted() {
-    this.scroll = new Bscroll(this.$refs.wrapper);
+    this.$nextTick(() => {
+      this.scroll = new Bscroll(this.$refs.wrapper);
+    });
   }
 };
 </script>
