923f538ea0ebf0fac96f21856b9343d4e3b4791c
unknown <liangjuncheng1995>
2020-05-03 16:52:42 +0800

    search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,67 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+    <div v-show="keyword" class="search-content" ref="search">
+      <ul>
+        <li class="search-item border-bottom" :key="item.id" v-for="item of list">{{item.name}}</li>
+        <li style="text-align: center" v-show="hasNoData" class="search-item border-bottom">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script type="text/javascript">
+import Bscroll from "@better-scroll/core";
 export default {
-  name: "CityHeader"
+  name: "CityHeader",
+  props: {
+    cities: Object
+  },
+  data() {
+    return {
+      keyword: "",
+      list: [],
+      timer: null
+    };
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length;
+    }
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.search);
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      if (!this.keyword) {
+        this.list = [];
+        return;
+      }
+      this.timer = setTimeout(() => {
+        const result = [];
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (
+              value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(value);
+            }
+          });
+        }
+        this.list = result;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      }, 100);
+    }
+  }
 };
 </script>
 
@@ -27,6 +82,24 @@ export default {
     text-align: center;
     border-radius: 0.06rem;
     color: #666666;
+  }
+}
+
+.search-content {
+  z-index: 1;
+  overflow: hidden;
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #eee;
+
+  .search-item {
+    line-height: 0.62rem;
+    padding-left: 0.2rem;
+    background: #fff;
+    color: #666;
   }
 }
 </style>
